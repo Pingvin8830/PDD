03138c8d80a544d4dd72734cb144c1af78dfb46c
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-10-15 12:22:37 +0300

    Исключение зацикливания

--- PDD/firefox.sh
+++ PDD/firefox.sh
@@ -4,10 +4,4 @@ cd /data/git/PDD/PDD
 ./PDD.py
 
 RES=`grep ^Result forklift.log | tail -1 | cut -f 2 -d ' '`
-
-while [ "${RES}" != 'OK' ]; do
-  ./PDD.py
-  RES=`grep ^Result forklift.log | tail -1 | cut -f 2 -d ' '`
-  done
-
-firefox
+[ "${RES}" = 'OK' ] && firefox
