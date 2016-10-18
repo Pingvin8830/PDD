18ca23d3fb997d85509639b1e4b41d507f65d526
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-10-18 22:51:27 +0300

    Сделал гадость - сердцу радость

--- PDD/firefox.sh
+++ PDD/firefox.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 cd /data/git/PDD/PDD
-./exam.py
+./exam.py -c configs/forklift.conf
 
 RES=`grep ^Result logs/forklift.log | tail -1 | cut -f 2 -d ' '`
+if [ "${RES}" != 'OK' ]; then
+  notify-send "Нет билета - нет интернета."
+  exit
+  fi
+
+./exam.py -c configs/expluatation.conf
+RES=`grep ^Result logs/expluatation.log | tail -1 | cut -f 2 -d ' '`
 [ "${RES}" = 'OK' ] && firefox
