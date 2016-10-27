8e195e40dc12ac0a2c508d347b3d986dce97cf1e
Vlasov Anton <Vlasov-Ant@yandex.ru>
2016-10-27 09:21:10 +0300

    Экзамен по ПДД

--- PDD/firefox.sh
+++ PDD/firefox.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd /data/git/PDD/PDD
-./exam.py -c configs/forklift.conf
+./exam.py -c configs/PDD.conf
 
-RES=`grep ^Result logs/forklift.log | tail -1 | cut -f 2 -d ' '`
+RES=`grep ^Result logs/PDD.log | tail -1 | cut -f 2 -d ' '`
 if [ "${RES}" != 'OK' ]; then
   notify-send "Нет билета - нет интернета."
   exit
